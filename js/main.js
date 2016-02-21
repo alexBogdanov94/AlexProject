4865118cb53d48ca96cb514ffadb408073fae406
Alex Bogdanov <alexander.bogdanov.eng@gmail.com>
2016-02-21 22:56:26 +0200

    changes saved

--- js/main.js
+++ js/main.js
@@ -7,25 +7,19 @@ width: 'toggle'
 },1000);
 
 
-
+$(this).text($(this).text() == 'open me' ? 'close me' : 'open me');
 
 	
 
 });//end click
-    var close = "close me";
-    var open = "open me";
+   
 $("#openMob").click(function(){
         $("#content").slideToggle("slow");
     $(this).text($(this).text() == 'open me' ? 'close me' : 'open me');
   
     });//end openMob
-    var close = "close me";
-    var open = "open me";
-$('#door p').click(function(){
-$(this).text($(this).text() == 'open me' ? 'close me' : 'open me');
- 
     
-});//end change text
+
 });
 
 
